d8cd43445aabb8e34dd4d8733be8fdd7c61dff81
paranoe <is-21-4-6@permaviat.ru>
2025-02-14 00:48:53 +0500

    Реализовал классы контекстов в папке Classes. Дружинин

--- WpfApp7/ViewModel/VM_Pages.cs
+++ WpfApp7/ViewModel/VM_Pages.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WpfApp7.Classes;
 
 namespace WpfApp7.ViewModel
 {
-    internal class VM_Pages
+    public class VM_Pages : Notification
     {
+        public VM_Tasks vm_sotrud = new VM_Tasks();
+
+        public VM_Categorys vm_groups = new VM_Categorys();
+
+        public VM_Pages() => MainWindow.init.frame.Navigate(new View.Tasks.Main(vm_Tasks));
+
+        public RealyCommand OpenTasks
+        {
+            get => new RealyCommand(obj => MainWindow.init.frame.Navigate(new View.Tasks.Main(vm_tasks)));
+        }
+
+        public RealyCommand OpenCatergorys
+        {
+            get => new RealyCommand(obj => MainWindow.init.frame.Navigate(new View.Categorys.Main(vm_categorys)));
+        }
     }
 }
